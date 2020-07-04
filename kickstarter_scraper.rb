1ff6fe5ece4d1e0204ce3b6a56783a45f2df6d8d
Lisa Feng <lisafeng10@gmail.com>
2020-07-04 14:45:21 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,4 @@
 require 'nokogiri'
-require 'pry'
 
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
@@ -18,12 +17,10 @@ def create_project_hash
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
-      :description => project.css("p.bbcard_blurb").text,
+      :description => project.css("p.bbcard_blurb").text.gsub("\n",""),
       :location => project.css("ul.project-meta span.location-name").text,
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
   end
   projects
 end
-
-create_project_hash
